23072391da792238c662bd21ae9b52e575e75fe0
Jaden-77 <131693026+Jaden-77@users.noreply.github.com>
2023-09-08 10:04:36 +0800

    Add files via upload

---
+++ EmployeeProject/src/employee_version1/EmployeeProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee_version1;
+
+/**
+ *
+ * @author User
+ */
+public class EmployeeProject {
+
+   
+    public static void main(String[] args) {
+         
+    }
+    
+}
